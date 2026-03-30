29e221efe7cb4eb3ecbd3b72f4d411072b35bf82
farsica <fariborz2004@yahoo.com>
2026-03-31 00:43:33 +0330

    Lang

--- src/Infrastructure/Infrastructure/Migrations/20260324060613_Languages.cs
+++ src/Infrastructure/Infrastructure/Migrations/20260324060613_Languages.cs
@@ -14,7 +14,7 @@ namespace GamaEdtech.Infrastructure.Migrations
                 name: "Languages",
                 columns: table => new
                 {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
+                    Id = table.Column<long>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
                     Code = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
