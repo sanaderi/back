a4fcd9d44335090cb5f68f8f72673564292aff3e
farsica <fariborz2004@yahoo.com>
2026-05-16 23:25:17 +0330

    Stripe Checkout Requests Email from Users #426

--- src/Core/Data/Dto/Provider/PaymentGateway/CreateRequestDto.cs
+++ src/Core/Data/Dto/Provider/PaymentGateway/CreateRequestDto.cs
@@ -10,5 +10,6 @@ namespace GamaEdtech.Data.Dto.Provider.PaymentGateway
         public required long PaymentId { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
+        public string? Email { get; set; }
     }
 }
